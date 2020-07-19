62ee5a310e0eb6956f2c8e9dc7d9c3d5f8133aa5
Angus Macrae <angusdmacrae@gmail.com>
2020-07-19 15:48:38 +0800

    Add home route for landing page

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,26 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Order from '../views/Order.vue';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/order',
     name: 'Order',
-    component: Order,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "order" */ '../views/Order.vue'),
   },
   {
     path: '/pay',
     name: 'Pay',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "pay" */ '../views/Pay.vue'),
   },
 ];
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>some text</p>
+    <a @click="goToOrderPage()">Order now!</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    goToOrderPage() {
+      this.$router.push({ path: '/order' });
+    },
+  },
+};
+</script>
