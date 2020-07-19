946d754f300d5ffd687afad3d4f837fe13be33fc
Angus Macrae <angusdmacrae@gmail.com>
2020-07-19 18:15:31 +0800

    Refactor drink image constants and change to JSON format

--- src/components/DrinkThumbnail.vue
+++ src/components/DrinkThumbnail.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import { drinkImageConstants } from '../data/drinkImageConstants';
+import drinkImageConstants from '../data/drinkImageConstants.json';
 import rough from 'roughjs/bundled/rough.cjs';
 
 export default {
@@ -38,67 +38,54 @@ export default {
       this.drawCup();
     },
     drawStraw() {
-      let svgPath = drinkImageConstants.straw[this.strawDirection][this.options.size];
-      let svgOptions = {
+      const svgPath = drinkImageConstants.straw[this.strawDirection][this.options.size];
+      const svgOptions = {
         strokeWidth: '1',
         stroke: 'rgb(60,60,60)',
       };
-      let straw = this.roughDraw.path(svgPath, svgOptions);
+      const straw = this.roughDraw.path(svgPath, svgOptions);
       straw.classList.add('straw');
       this.$el.appendChild(straw);
     },
     drawTea() {
-      let svgPath = drinkImageConstants.tea[this.options.size];
-      let svgOptions = {
+      const svgPath = drinkImageConstants.tea[this.options.size];
+      const svgOptions = {
         fill: drinkImageConstants.teaColor[this.options.tea],
         fillStyle: 'solid',
         strokeWidth: 0,
         roughness: 0,
       };
-      let tea = this.roughDraw.path(svgPath, svgOptions);
+      const tea = this.roughDraw.path(svgPath, svgOptions);
       tea.classList.add('tea');
       this.$el.appendChild(tea);
     },
     drawMilk() {
       if (this.options.milk == 0) {
-        let svgPath = drinkImageConstants.milk[this.options.size];
-        let svgOptions = {
+        const svgPath = drinkImageConstants.milk[this.options.size];
+        const svgOptions = {
           fill: 'rgba(255,250,250,0.9)',
           fillStyle: 'solid',
           strokeWidth: 0,
           roughness: 0,
         };
-        let milkCap = this.roughDraw.path(svgPath, svgOptions);
-        milkCap.classList.add('milk');
-        this.$el.appendChild(milkCap);
+        const milk = this.roughDraw.path(svgPath, svgOptions);
+        milk.classList.add('milk');
+        this.$el.appendChild(milk);
       }
     },
     drawIce() {
-      switch (this.options.ice) {
-        case 2:
-          break;
-        case 1:
-          this.drawIceCubes(0, 2);
-          break;
-        case 0:
-          this.drawIceCubes(0, 4);
-          break;
-      }
-    },
-    drawIceCubes(firstCubeIndex, lastCubeIndex) {
-      for (let i = firstCubeIndex; i <= lastCubeIndex; i++) {
-        let svgPath = drinkImageConstants.ice[this.options.size][i];
-        let svgOptions = {
+      drinkImageConstants.ice[this.options.size][this.options.ice].forEach(svgPath => {
+        const svgOptions = {
           fill: 'rgba(245,245,255,0.95)',
           fillStyle: 'solid',
           stroke: 'rgba(60,60,60,0.3)',
           strokeWidth: 0.5,
           roughness: 0.5,
         };
-        let iceCube = this.roughDraw.path(svgPath, svgOptions);
+        const iceCube = this.roughDraw.path(svgPath, svgOptions);
         iceCube.classList.add('iceCube');
         this.$el.appendChild(iceCube);
-      }
+      });
     },
     drawTopping() {
       switch (this.options.topping) {
@@ -111,61 +98,57 @@ export default {
       }
     },
     drawPearls() {
-      let lastPearlIndex = drinkImageConstants.topping.pearls.length;
-      let lastPearlToDraw = lastPearlIndex - (this.options.size == 0 ? 6 : 0);
-      for (let i = 0; i < lastPearlToDraw; i++) {
-        let svgCircle = [...drinkImageConstants.topping.pearls[i], 7];
-        let svgOptions = {
+      drinkImageConstants.topping.pearls[this.options.size].forEach(circleParameters => {
+        const svgCircle = [...circleParameters, 7];
+        const svgOptions = {
           fill: 'rgba(37,23,26,0.9)',
           strokeWidth: 0,
           roughness: 0.3,
           fillStyle: 'solid',
         };
-        let pearl = this.roughDraw.circle(...svgCircle, svgOptions);
+        const pearl = this.roughDraw.circle(...svgCircle, svgOptions);
         pearl.classList.add('topping');
         pearl.classList.add('pearl');
         this.$el.appendChild(pearl);
-      }
+      });
     },
     drawJelly() {
-      let lastJellyIndex = drinkImageConstants.topping.coconutJelly.length;
-      let lastJellyToDraw = lastJellyIndex - (this.options.size == 0 ? 3 : 0);
-      for (let i = 0; i < lastJellyToDraw; i++) {
-        let svgRectangle = [...drinkImageConstants.topping.coconutJelly[i], 12, 6];
-        let svgOptions = {
+      drinkImageConstants.topping.jelly[this.options.size].forEach(rectangleParameters => {
+        const svgRectangle = [...rectangleParameters, 12, 6];
+        const svgOptions = {
           fill: 'rgba(245,255,245,0.95)',
           strokeWidth: 0.5,
           stroke: 'rgba(60,60,60,0.3)',
           roughness: 0.4,
           fillStyle: 'solid',
         };
-        let jelly = this.roughDraw.rectangle(...svgRectangle, svgOptions);
+        const jelly = this.roughDraw.rectangle(...svgRectangle, svgOptions);
         jelly.classList.add('topping');
         jelly.classList.add('jelly');
         this.$el.appendChild(jelly);
-      }
+      });
       // TODO: draw slanted jelly in the same loop as normal jelly?
       // adjuster = this.options.size == 0 ? 1 : 0;
       // for (
       //   let i = 0;
-      //   i < drinkImageConstants.topping.coconutJellySlanted.length - adjuster;
+      //   i < drinkImageConstants.topping.jellySlanted.length - adjuster;
       //   i++
       // ) {
-      //   let svgPath = drinkImageConstants.topping.coconutJellySlanted[i];
-      //   let svgOptions = {
+      //   const svgPath = drinkImageConstants.topping.jellySlanted[i];
+      //   const svgOptions = {
       //     fill: "rgba(245,255,245,0.95)",
       //     strokeWidth: 0.5,
       //     stroke: "rgba(60,60,60,0.3)",
       //     roughness: 0.4,
       //     fillStyle: "solid"
       //   };
-      //   let jelly = this.roughDraw.path(svgPath, svgOptions);
+      //   const jelly = this.roughDraw.path(svgPath, svgOptions);
       //   this.$el.appendChild(jelly);
       //   this.$el.lastElementChild.classList.add("jelly-slanted" + i);
       // }
     },
     drawCup() {
-      let cup = this.roughDraw.path(drinkImageConstants.cup[this.options.size], {
+      const cup = this.roughDraw.path(drinkImageConstants.cup[this.options.size], {
         stroke: 'rgb(60,60,60)',
       });
       cup.classList.add('cup');
