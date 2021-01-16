e4257f4b48e9f75148ff3d3c4a92bbc48df3f6b7
Angus Macrae <angusdmacrae@gmail.com>
2021-01-16 20:42:29 +0000

    Refactor nextArrayIndex using modulus operator

--- src/functions/nextArrayIndex.js
+++ src/functions/nextArrayIndex.js
@@ -1,7 +1,3 @@
 export default function nextArrayIndex(currentIndex, array) {
-  if (currentIndex == array.length - 1) {
-    return 0;
-  } else {
-    return +currentIndex + 1;
-  }
+  return ++currentIndex % array.length;
 }
