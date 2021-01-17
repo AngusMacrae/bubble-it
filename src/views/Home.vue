af0ab194fd2f89050e2905aa36b4369e8a624497
Angus Macrae <angusdmacrae@gmail.com>
2021-01-17 20:59:18 +0000

    Add splash image and basic styling for Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-    <p>some text</p>
-    <a @click="goToOrderPage()">Order now!</a>
+    <LandingSplash />
+    <p>Fresh bubble tea at your door in 15 mins or it's <strong>free!</strong></p>
+    <a @click="goToOrderPage()">
+      <SketchedBox :fillColour="fillColour" class="filled-box" />
+      <SketchedBox class="empty-box" />
+      Order now!
+    </a>
   </div>
 </template>
 
 <script>
+import LandingSplash from '../components/home/LandingSplash.vue';
+import SketchedBox from '../components/SketchedBox.vue';
+import themeColours from '../data/themeColours.json';
+
 export default {
+  components: { LandingSplash, SketchedBox },
   name: 'Home',
+  data() {
+    return {
+      fillColour: themeColours[0].fill,
+    };
+  },
   methods: {
     goToOrderPage() {
       this.$router.push({ path: '/order' });
@@ -15,3 +30,31 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+p {
+  text-align: center;
+  margin: 2rem 0;
+}
+
+a {
+  display: block;
+  position: relative;
+  text-align: center;
+  padding: 1em 3em;
+  width: max-content;
+  margin: 0 auto;
+}
+
+.filled-box {
+  opacity: 0;
+}
+
+a:hover .filled-box {
+  opacity: 0.5;
+}
+
+a:hover .empty-box {
+  opacity: 0;
+}
+</style>
